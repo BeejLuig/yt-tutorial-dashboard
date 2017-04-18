4517c4dc9dd01b208a9c8a5b9bb2a384b9fc6cc7
BeejLuig <bjcantlupe@gmail.com>
2017-04-18 19:37:05 -0400

    add jbuilder view and update UserController create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,15 +1,27 @@
 class Api::V1::UsersController < ApplicationController
 
   def create
-    token = "1234567890"
-    id = 1
-    username = params[:user][:username]
-    render json: {
-      token: token,
-      user: {
-        id: id,
-        username: username
-      }
-    }
+    # 1. create a user
+    @user = User.new(user_params)
+
+    if @user.save # -> if creation is successful
+      render 'users/user_with_token.json.jbuilder', user: @user
+        # 2. pass the user id to an auth module that creates a JWT token
+        # 3. we want to return the user boject and the token as json to the client
+    else # -> if creation is invalid
+      # 2. return the Active Record error messages as json
+      # 3. set the status to 500
+      render json: {
+        errors: @user.errors
+      }, status: 500
+    end
+    # -> if creation is successful
+      # 2. pass the user id to an auth module that creates a JWT token
+      # 3. we want to return the user boject and the token as json to the client
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 end
