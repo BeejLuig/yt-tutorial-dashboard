94aecfa86ad33f5fbad8227a921cf72a6550f969
BeejLuig <bjcantlupe@gmail.com>
2017-04-19 18:54:15 -0400

    add helper methods in ApplicationController
    added methods:
    - authenticate_user
    - current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+
+  helper_method :authenticate_token, :current_user
+
+  private
+
+  def authenticate_token!
+    #TODO
+  end
+
+  def current_user
+    #TODO
+  end
+
 end
