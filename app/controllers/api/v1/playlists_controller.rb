75d2c7071645a13cae284985ba9a79b02de3b8d9
BeejLuig <bjcantlupe@gmail.com>
2017-04-20 19:58:33 -0400

    Passing #index test for PlaylistsController

--- app/controllers/api/v1/playlists_controller.rb
+++ app/controllers/api/v1/playlists_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::PlaylistsController < ApplicationController
-  before_action :authenticate_token!, only: [:index, :create]
+  before_action :authenticate_token!, only: [:index, :create, :show]
 
   def index
-
+    render 'playlists/playlists.json.jbuilder', playlists: current_user.playlists
   end
 
   def create
