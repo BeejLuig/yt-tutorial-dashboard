f06af6c6ea2bad4d4dd28999ba2193e18a3a4464
BeejLuig <bjcantlupe@gmail.com>
2017-04-20 21:14:34 -0400

    add nested :videos attributes capability in Playlist

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,6 +1,8 @@
 class Playlist < ApplicationRecord
   belongs_to :user
+
   has_many :videos
+  accepts_nested_attributes_for :videos
 
   validates :title, :playlist_id, :thumbnail_url, presence: true
 end
