1c1072dccdb6ae22af07c9670cb977ef2f5c2eb1
BeejLuig <bjcantlupe@gmail.com>
2017-04-20 21:15:39 -0400

    Passing updated tests for #create action in PlaylistsController

--- app/controllers/api/v1/playlists_controller.rb
+++ app/controllers/api/v1/playlists_controller.rb
@@ -7,12 +7,10 @@ class Api::V1::PlaylistsController < ApplicationController
   end
 
   def create
-    @playlist = current_user.playlists.new(
-      title: params[:playlist][:title],
-      playlist_id: params[:playlist][:playlist_id],
-      description: params[:playlist][:description],
-      thumbnail_url: params[:playlist][:thumbnail_url]
-    )
+    @playlist = current_user.playlists.new(playlist_params)
+    @playlist.videos.each do |video|
+      video.playlist = @playlist
+    end
     if @playlist.save
       render 'playlists/playlist.json.jbuilder', playlists: @playlist
     else
@@ -24,6 +22,6 @@ class Api::V1::PlaylistsController < ApplicationController
 
   private
   def playlist_params
-    params.require(:playlist).permit(:title, :playlist_id, :description, :thumbnail_url, videos: [])
+    params.require(:playlist).permit(:title, :playlist_id, :description, :thumbnail_url, videos_attributes: [:title, :video_id, :description, :thumbnail_url])
   end
 end
