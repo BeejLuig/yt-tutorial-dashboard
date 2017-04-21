ca4e7d5a4019ebabb0ffe7fe18721424609fbd59
BeejLuig <bjcantlupe@gmail.com>
2017-04-20 23:00:46 -0400

    Passing first test for #index in VideosController

--- app/controllers/api/v1/videos_controller.rb
+++ app/controllers/api/v1/videos_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::VideosController < ApplicationController
 
   def index
+    playlist = Playlist.find_by(id: params[:playlist_id])
+    @videos = playlist.videos
+    render 'videos/videos.json.jbuilder', videos: @videos
   end
 end
