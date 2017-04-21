fe76f0938d3f6d57fdeffd4e21fa12675043bae0
BeejLuig <bjcantlupe@gmail.com>
2017-04-20 23:05:25 -0400

    Add error test for #index action in videos_request_spec.rb

--- spec/requests/api/v1/videos_request_spec.rb
+++ spec/requests/api/v1/videos_request_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe "Api::V1::Videos", type: :request do
   end
 
   describe "#index" do
-    it "it shows all videos belonging to a playlist" do
+    it "on success, shows all videos belonging to a playlist" do
       get "/api/v1/playlists/#{@playlist.id}/videos", headers: @token_headers
       expect(response.body.empty?).to eq(false)
       body = JSON.parse(response.body)
@@ -40,6 +40,14 @@ RSpec.describe "Api::V1::Videos", type: :request do
       expect(body.first["description"]).to eq("description1")
       expect(body.first["thumbnail_url"]).to eq("url1.jpg")
       expect(body.first["playlist_id"]).to eq(@playlist.id)
+    end
+
+    it "on failure, displays an error message: 'No videos found with the given playlist id'" do
+      get "/api/v1/playlists/0/videos", headers: @token_headers
+      body = JSON.parse(response.body)
+      expect(body).to eq(
+        {"errors" => { "videos" => ["No videos found with the given playlist id"] } }
+      )
     end
   end
 end
