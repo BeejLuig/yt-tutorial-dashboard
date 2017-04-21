0e8b21d534714b1b8c29f7e8a5eb76d56bdefac5
BeejLuig <bjcantlupe@gmail.com>
2017-04-20 23:00:17 -0400

    Add #index test for videos_request_spec.rb

--- spec/requests/api/v1/videos_request_spec.rb
+++ spec/requests/api/v1/videos_request_spec.rb
@@ -11,6 +11,12 @@ RSpec.describe "Api::V1::Videos", type: :request do
       username: "NewUser",
       password: "password"
     )
+
+    @playlist = @user.playlists.create(title: "title", playlist_id: "abcd123", description: "", thumbnail_url: "this.jpg")
+
+    @playlist.videos.create(title: "title1", video_id: "12345", description: "description1", thumbnail_url: "url1.jpg")
+    @playlist.videos.create(title: "title2", video_id: "23456", description: "description2", thumbnail_url: "url2.jpg")
+
     @token = Auth.create_token(@user.id)
     @token_headers = {
       'Accept': 'application/json',
@@ -24,6 +30,16 @@ RSpec.describe "Api::V1::Videos", type: :request do
   end
 
   describe "#index" do
-    pending "it shows all videos belonging to a playlist"
+    it "it shows all videos belonging to a playlist" do
+      get "/api/v1/playlists/#{@playlist.id}/videos", headers: @token_headers
+      expect(response.body.empty?).to eq(false)
+      body = JSON.parse(response.body)
+      expect(body.count).to eq(2)
+      expect(body.first["title"]).to eq("title1")
+      expect(body.first["video_id"]).to eq("12345")
+      expect(body.first["description"]).to eq("description1")
+      expect(body.first["thumbnail_url"]).to eq("url1.jpg")
+      expect(body.first["playlist_id"]).to eq(@playlist.id)
+    end
   end
 end
