72b512f99c1062a080c0a96f92348a53a80e3a42
BeejLuig <bjcantlupe@gmail.com>
2017-04-21 18:38:43 -0400

    Passing #reset_videos test

--- app/controllers/api/v1/videos_controller.rb
+++ app/controllers/api/v1/videos_controller.rb
@@ -13,4 +13,21 @@ class Api::V1::VideosController < ApplicationController
       }
     end
   end
+
+  def reset_videos
+    playlist = Playlist.find_by(id: params[:playlist_id])
+
+    if playlist
+      @videos = playlist.videos
+      @videos.update(complete?: false)
+      render 'videos/videos.json.jbuilder', videos: @videos
+    else
+      render json: {
+        errors: {
+          videos: ["No videos found with the given id"]
+        }
+      }
+    end
+  end
+
 end
