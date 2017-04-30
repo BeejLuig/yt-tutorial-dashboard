59b030568d0f1c8bccec0f13e1fb1b623829ad29
BeejLuig <bjcantlupe@gmail.com>
2017-04-30 15:59:04 -0400

    add instance methods #total_videos and #completed_videos

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,8 +1,17 @@
 class Playlist < ApplicationRecord
   belongs_to :user
+  attr_reader :total_videos, :completed_videos
 
   has_many :videos, dependent: :destroy
   accepts_nested_attributes_for :videos
 
   validates :title, :playlist_id, :thumbnail_url, :channel_title, presence: true
+
+  def total_videos
+    videos.length
+  end
+
+  def completed_videos
+    videos.select {|v| v.complete? }.length
+  end
 end
