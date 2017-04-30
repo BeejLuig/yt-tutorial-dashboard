297cd1bbec0b963589a018f2dd1bd45b7192c3eb
BeejLuig <bjcantlupe@gmail.com>
2017-04-30 15:56:14 -0400

    add is_active status to Videos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170427234336) do
+ActiveRecord::Schema.define(version: 20170429204623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,10 @@ ActiveRecord::Schema.define(version: 20170427234336) do
     t.integer  "playlist_id"
     t.datetime "created_at",                    null: false
     t.datetime "updated_at",                    null: false
+    t.boolean  "is_active",     default: false
     t.index ["playlist_id"], name: "index_videos_on_playlist_id", using: :btree
   end
 
+  add_foreign_key "playlists", "users"
+  add_foreign_key "videos", "playlists"
 end
